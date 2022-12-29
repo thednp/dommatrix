36b4bbee66d82f2d6435b497b9971a0eb8b71af7
thednp <dnp.digital@gmail.com>
2022-12-29 20:57:10 +0200

    remove old types

--- types/index.d.ts
+++
@@ -1,6 +0,0 @@
-export as namespace CSSM;
-export default CSSMatrix;
-
-export { PointTuple, JSONMatrix, matrix, matrix3d } from './more/types';
-
-import { default as CSSMatrix } from 'dommatrix/src/dommatrix';
--- types/more/dommatrix.ts
+++
@@ -1,2 +0,0 @@
-export {default as CSSMatrix} from '../../src/dommatrix';
-export {default as Version} from '../../src/version';
