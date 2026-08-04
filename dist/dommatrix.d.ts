a70709ad408c4a0a9a1d8ca14dde7f74e0a67ffe
thednp <dnp.digital@gmail.com>
2026-08-04 09:42:22 +0300

    more jsdoc

--- dist/dommatrix.d.ts
+++ dist/dommatrix.d.ts
@@ -1,36 +1,64 @@
 //#region src/types.d.ts
-/** A DOMMPoint compatible Tuple. */
+/** A DOMPoint compatible Tuple. */
 interface PointTuple {
+  /** The X coordinate of the point. */
   x: number;
+  /** The Y coordinate of the point. */
   y: number;
+  /** The Z coordinate of the point. */
   z: number;
+  /** The perspective divide component of the point. */
   w: number;
 }
 /** The result of **CSSMatrix.toJSON()** / **DOMMatrix.toJSON()** instance calls. */
 interface JSONMatrix {
+  /** The first element of the first row, alias of `a`. */
   m11: number;
+  /** The second element of the first row, alias of `b`. */
   m12: number;
+  /** The third element of the first row. */
   m13: number;
+  /** The fourth element of the first row. */
   m14: number;
+  /** The first element of the second row, alias of `c`. */
   m21: number;
+  /** The second element of the second row, alias of `d`. */
   m22: number;
+  /** The third element of the second row. */
   m23: number;
+  /** The fourth element of the second row. */
   m24: number;
+  /** The first element of the third row. */
   m31: number;
+  /** The second element of the third row. */
   m32: number;
+  /** The third element of the third row. */
   m33: number;
+  /** The fourth element of the third row. */
   m34: number;
+  /** The first element of the fourth row, alias of `e`. */
   m41: number;
+  /** The second element of the fourth row, alias of `f`. */
   m42: number;
+  /** The third element of the fourth row. */
   m43: number;
+  /** The fourth element of the fourth row. */
   m44: number;
+  /** The first element of the first row, alias of `m11`. */
   a: number;
+  /** The second element of the first row, alias of `m12`. */
   b: number;
+  /** The first element of the second row, alias of `m21`. */
   c: number;
+  /** The second element of the second row, alias of `m22`. */
   d: number;
+  /** The first element of the fourth row, alias of `m41`. */
   e: number;
+  /** The second element of the fourth row, alias of `m42`. */
   f: number;
+  /** A boolean flag indicating whether the matrix is 2D. */
   is2D: boolean;
+  /** A boolean flag indicating whether the matrix is the identity matrix. */
   isIdentity: boolean;
 }
 /** An array of 6 numbers representing a 2D matrix. */
